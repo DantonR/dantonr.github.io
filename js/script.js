7f2af8e258bdc00ad67894405cba68ae5e1dba86
DantonR <danton.ruthe@gmail.com>
2018-06-12 14:36:53 +1200

    12 June Further HTML & CSS  development

--- js/script.js
+++ js/script.js
@@ -11,5 +11,49 @@ zoom: 15
 
 // pagepiling
 $(document).ready(function() {
-	$('#pagepiling').pagepiling();
+	$('#pagepiling').pagepiling({
+		verticalCentered: false,
+		scrollingSpeed: 200,
+	});
 });
+
+
+// TEST JS IN THIS FUNCTION
+var testingJs = function(){
+	// PREVENT SCROLL TEST
+	var mapBoxDiv = document.getElementById('map');
+	console.log('test is working');
+
+	document.addEventListener('mousewheel DOMMouseScroll', function() {
+	        console.log('scrolling');
+	});
+	// PREVENT SCROLL TEST END
+}();
+
+
+
+// function preventScroll(e) {
+// 	console.log('this is scroll');
+// 	if (e.target.id === 'map') {
+// 		console.log('hahah');
+// 		$.fn.pagepiling.setAllowScrolling(false);
+// 	} else {
+// 		$.fn.pagepiling.setAllowScrolling(true);
+// 	}
+// }
+
+
+// APP START
+var app = {
+	vars: {
+		mapBoxDiv: document.getElementById('map'),
+	},
+	eventListeners: function() {
+		app.vars.mapBoxDiv.addEventListener('scroll', function(){
+			event.preventDefault();
+		}, false)
+	}
+} // APP END
+
+console.dir(app.vars.mapBoxDiv);
+// app.eventListeners();
