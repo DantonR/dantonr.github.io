50d9f0ddc233c20d112af3a064af58fcbb7398c0
DantonR <danton.ruthe@gmail.com>
2018-06-20 08:41:57 +1200

    Morning

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 // test your own javascript within this function
 var testingJs = function(){
 	//get directions
-
-	directions.on('route', function(directions){
-		console.log(directions.route['0'].distance / 1000);
-		distance.innerHTML = directions.route["0"].distance / 1000;
-	});
-
-
 }();
 
-
-
+directions.on('route', function(directions){
+	// console.log(directions.route['0'].distance / 1000);
+	// distance.innerHTML = directions.route["0"].distance / 1000;
+	app.vars.mapboxDistance.push(directions.route["0"].distance);
+	console.log(app.vars.mapboxDistance);
+});
 
 // APP START
 var app = {
@@ -20,25 +17,55 @@ var app = {
 		body: document.querySelector('body'),
 		mapBoxDiv: document.getElementById('map'),
 		scrollBtns: document.querySelectorAll('.btn-primary'),
-		seatsNeeded: document.getElementById('seatsNeeded'),
+
+		// input fields
 		startDate: document.getElementById('startDate'),
 		endDate: document.getElementById('endDate'),
+		seatsNeeded: document.getElementById('seatsNeeded'),
+
+		// the arrays that will contain the dates & days traveling
+		dates: ['itemOne', 'itemTwo'],
+		daysTraveling: ['a'],
+
+		// array containing map data
+		mapboxDistance: [],
+
+		// the divs containing the different vehicle options
 		bike: document.getElementById('bike'),
 		smallCar: document.getElementById('smallCar'),
 		largeCar: document.getElementById('largeCar'),
 		motorhome: document.getElementById('motorhome'),
-		dates: ['itemOne', 'itemTwo'],
-		daysTraveling: ['a']
+
+
+		// final display of information
+		infoDisplay: document.getElementById('infoDisplay'),
+		infoPickup: document.getElementById('infoPickup'),
+		infoDropoff: document.getElementById('infoDropoff'),
+		infoDays: document.getElementById('infoDays'),
+		infoDistance: document.getElementById('infoDistance'),
+		infoVehicle: document.getElementById('infoVehicle'),
+		infoCost: document.getElementById('infoCost')
 	},
 	// ** variables end **
 
 	// **event listeners**
 	eventListeners: function() {
 
+		// * page 2 button press *
+		app.vars.scrollBtns[1].addEventListener('click', function(){
+			console.log(app.vars.mapboxObj);
+		}, false)
+
 		// * page 3 button press *
 		app.vars.scrollBtns[2].addEventListener('click', function(){
 			app.getDates(app.vars.startDate, app.vars.endDate);
 			app.showVehicles(parseInt(app.vars.seatsNeeded.value), app.vars.daysTraveling[0] );
+		}, false);
+
+		// * page 4 button press *
+		app.vars.scrollBtns[3].addEventListener('click', function(){
+			var v = app.vars;
+			app.displayInfo(v.startDate, v.endDate, v.daysTraveling[0]);
 		}, false);
 
 		// *up & down buttons*
@@ -56,6 +83,12 @@ var app = {
 
 	// ** functions **
 
+	// get map data
+	getMapData: function() {
+
+	},
+
+
 	// take the dates given and calculate the amount of days traveling
 	getDates: function(b, a) {
 		app.vars.dates.splice(0, 2);
@@ -66,6 +99,7 @@ var app = {
 		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 		app.vars.daysTraveling.splice(0, 1);
 		app.vars.daysTraveling.push(diffDays);
+		console.log(app.vars.dates);
 		console.log(app.vars.daysTraveling[0]);
 	}, // get dates end
 
@@ -95,7 +129,25 @@ var app = {
 		} else {
 			app.vars.motorhome.style.display = 'none';
 		}
-	} // show vehicles end
+	}, // show vehicles end
+
+	// change the info on the last page based on the users input + data
+	displayInfo: function(pickup, dropoff, days) {
+		var v = app.vars;
+		v.infoPickup.innerHTML = pickup.value;
+		v.infoDropoff.innerHTML = dropoff.value;
+		v.infoDays.innerHTML = days;
+	},
+
+	// mapbox function
+
+
+
+
+
+
+
+
 
 	// ** functions end **
 
@@ -104,6 +156,10 @@ var app = {
 
 }; // APP END
 
+
+
+
 app.eventListeners();
 
 // CONSOLE LOGS
+console.dir(app.displayInfo());
