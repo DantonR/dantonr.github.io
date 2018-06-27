c6257a5a950bad38fa8e50c477c5d4b806ac1927
DantonR <danton.ruthe@gmail.com>
2018-06-27 16:27:29 +1200

    Working, need to put test code into object-- 27th June

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@
 directions.on('route', function(directions){
 	app.vars.mapboxDistance.push(directions.route["0"].distance / 1000);
 });
-console.log($('.btn-primary'));
 // APP START
 var app = {
 	// --------------------------------
@@ -41,6 +40,9 @@ var app = {
 		pageFourDiv: document.getElementsByClassName('page-four__content')[0],
 		cards: document.getElementsByClassName('page-4__card'),
 
+		// page 5 card
+		pageFiveCard: document.getElementsByClassName('page-five__card')[0],
+
 		// final display of information
 		infoDisplay: document.getElementById('infoDisplay'),
 		infoPickup: document.getElementById('infoPickup'),
@@ -62,16 +64,11 @@ var app = {
 		var v = app.vars;
 		// * home page button press
 		v.scrollBtns[0].addEventListener('click', function(){
-			// $.fn.pagepiling.moveSectionDown();
 		}, false);
 
 		// * page 2 button press *
 		v.scrollBtns[1].addEventListener('click', function(){
-			// if (typeof v.mapboxDistance[0] === 'number') {
-			// 	$.fn.pagepiling.moveSectionDown();
-			// } else {
-			// 	alert('You havent said where youre going!');
-			// }
+
 		}, false);
 
 		// * page 3 button press *
@@ -83,7 +80,6 @@ var app = {
 
 		// *up & down buttons*
 		v.body.addEventListener('click', function(e){
-			console.dir(e.target);
 
 			if(e.target.className === 'btn btn-primary') {
 				$.fn.pagepiling.moveSectionDown();
@@ -131,80 +127,109 @@ var app = {
 //  	   CALLING
 // -------------------------
 app.eventListeners();
-	console.dir(vehicles.motorhome.seats);
 
 
 
 // -------------------------
 //     CONSOLE LOGS
 // -------------------------
+
 // -------------------------
 //          TEST JS
 // -------------------------
 var testingJs = function(){
 	var v = app.vars;
 
+	var page = $(".page-four__content")[0]
+	page.addEventListener('click', catTest, false);
+
+	function catTest(e) {
+		console.dir(e.target);
+	}
+
+	// expand the vehicle div when clicked on, and hide the others
 	$(".page-four__content").on('click', '.page-4__card', function(){
 		$(this).toggleClass('page-4__card--large').siblings().toggleClass('page-4__card--transparent');
 		v.selectedVehicle.splice(0, 1);
 		v.selectedVehicle.push(this.id);
-		// console.dir(v.selectedVehicle);
-
 	});
 
 
+	// show the vehicles depending on inputs
 	$('#scrollBtnThree').click(function(){
-		console.log(v.seatsNeeded.value);
 		app.getDates(v.startDate, v.endDate);
-		compareData('motorbike', vehicles.motorbike, parseInt(v.seatsNeeded.value), v.daysTraveling[0] )
-		compareData('smallCar', vehicles.smallCar, parseInt(v.seatsNeeded.value), v.daysTraveling[0] )
-		compareData('largeCar', vehicles.largeCar, parseInt(v.seatsNeeded.value), v.daysTraveling[0] )
-		compareData('motorhome', vehicles.motorhome, parseInt(v.seatsNeeded.value), v.daysTraveling[0] )
+		compareData('motorbike', vehicles.motorbike, parseInt(v.seatsNeeded.value), v.daysTraveling[0], 'Motorbike' );
+		compareData('smallCar', vehicles.smallCar, parseInt(v.seatsNeeded.value), v.daysTraveling[0], 'Small Car' );
+		compareData('largeCar', vehicles.largeCar, parseInt(v.seatsNeeded.value), v.daysTraveling[0], 'Large Car' );
+		compareData('motorhome', vehicles.motorhome, parseInt(v.seatsNeeded.value), v.daysTraveling[0], 'Motorhome'  );
 	});
 
+	// when selecting go back on the fourth page, delete the current vehicles in the page
+	$('#fourthBackBtn').on('click', function(){
+		deleteChildren();
+	});
 
-	function compareData(objName, obj, seats, days){
+	// when selecting go back on the fourth page, delete the current vehicles in the page
+	function deleteChildren(){
+		var p = app.vars.pageFourDiv
+		var l = p.childNodes.length - 1;
+		console.log(l);
+
+		for(var i = 2; i < l; i++){
+			console.log(i);
+			p.removeChild(p.childNodes[2]);
+		}
+	};
+
+	// take the data given and create vehicles based on that
+	function compareData(objName, obj, seats, days, displayName){
 		var p4 = app.vars.pageFourDiv;
-		console.log(seats[0]);
-		console.log(days);
-		console.dir(obj);
+		var v = app.vars;
+		var fuelCost =  Math.ceil( ((v.mapboxDistance[0] / 100) * obj.fuel) * 2.50 );
+		var totalCost = fuelCost + (days * obj.cost);
+		var multipleSeats = '<p class="light">For <strong>' + obj.seats[0] + ' - ' + obj.seats[1] + '</strong> People </p>';
+		if (objName === 'motorbike') {
+			multipleSeats = '<p class="light">For <strong>' + obj.seats[0] + '</strong> Person </p>';
+		}
 		if(seats >= obj.seats[0] &&  seats <= obj.seats[1] && days >= obj.days[0] && days <= obj.days[1]) {
 			var newVehicle = '<div class="page-4__card" id=" '  + objName + ' ">';
 			newVehicle += 		'<img class="card__img center-align" src="img/' + objName + '.png" alt="Two seater car" />';
-			newVehicle += 		'<h2 class="center-align green">' + objName + '</h2>';
+			newVehicle += 		'<h2 class="center-align green">' + displayName + '</h2>';
 			newVehicle += 		'<div>';
 			newVehicle += 			'<p class="center-align">Lorem ipsum dolor sit</p>';
-			newVehicle += 			'<p class="light">For <strong>' + obj.seats[0] + '</strong> Person</p>';
+			newVehicle += 			multipleSeats;
 			newVehicle += 			'<p class="light"><strong>$' + obj.cost + '</strong> per day</p>';
 			newVehicle += 			'<p class="margin-before green"><strong>Costs for your trip:</strong></p>';
-			newVehicle += 			'<p class="light">Fuel (approx): <span class="right-align bold">$x</span></p>';
+			newVehicle += 			'<p class="light">Fuel (approx): <span class="right-align bold">$' + fuelCost + '</span></p>';
 			newVehicle += 			'<p class="light">Vehicle hire<span class="right-align bold">$' + days * obj.cost + '</span></p>';
 			newVehicle += 			'<hr>';
-			newVehicle += 			'<p>Total<span class="right-align">$x</span></p>';
+			newVehicle += 			'<p>Total<span class="right-align">$' + totalCost +'</span></p>';
 			newVehicle += 		'</div>';
-			newVehicle += 		'<button class="btn btn-tertiary center-align confirm" id=" ' + objName + 'Btn ">Confirm</button>';
+			newVehicle += 		'<button class="btn btn-tertiary center-align confirm" id="' + objName + 'Btn">Confirm</button>';
 			newVehicle += 	'</div>';
 
-			console.log(newVehicle);
-			console.log(app.vars.pageFourDiv.innerHTML);
 			var choose = document.getElementById('choose');
 			choose.insertAdjacentHTML('afterend', newVehicle);
 		}
 	};
 
-	confirmVehicle('motorhome')
 
-	function confirmVehicle(button) {
-		var confirmBtn = document.getElementById(button);
-		confirmBtn.addEventListener('click', function(){
-			console.log(button);
-		}, false)
+	// the confirm buttons on the expanded vehicle options
+	chooseVehicle('#motorbikeBtn', 'Motorbike', vehicles.motorbike);
+	chooseVehicle('#smallCarBtn', 'Small Car');
+	chooseVehicle('#largeCarBtn', 'Large Car');
+	chooseVehicle('#motorhomeBtn', 'Motorhome');
+
+
+	// show the final page data
+	function chooseVehicle(buttonId, vehicleName, dataItem){
+		$(document).on('click', buttonId, function(){
+			$.fn.pagepiling.moveSectionDown();
+			setTimeout(function(){
+				app.vars.pageFiveCard.style.transform = 'translateY(20px)';
+			}, 700);
+			v.infoVehicle.innerHTML = vehicleName;
+			v.infoCost.innerHTML = dataItem.cost;
+		});
 	};
-
-	// var motorhome = $('#motorhome')[0].children[2]
-	// var fuelM = motorhome.children[4].children[0]
-	// var vhireM = motorhome.children[5].children[0]
-	// var totelM = motorhome.children[7].children[0]
-	//
-	// .text()
 }();
